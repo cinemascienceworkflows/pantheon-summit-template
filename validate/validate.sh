772035fc9f0f6c251539c9535c29154d00450d44
David Rogers <dhr@lanl.gov>
2021-03-21 17:49:14 -0400

    update

--- validate/validate.sh
+++ validate/validate.sh
@@ -4,5 +4,5 @@ source ./pantheon/env.sh > /dev/null 2>&1
 
 echo "------------------------------------------------------------"
 echo "PTN: Validating" 
-echo "PTN: PASS
+echo "PTN: PASS"
 echo "------------------------------------------------------------"
